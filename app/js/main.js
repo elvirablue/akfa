b854a47729ce0a0ab13adff6d8a18e916cf7f972
Yuri Krutilov <ronnie57x@gmail.com>
2015-12-28 18:47:24 +0600

    front-page desctop

---
+++ app/js/main.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+  $(".front-page-slider").owlCarousel({
+  	animateOut: 'fadeOut',
+    animateIn: 'fadeIn',
+  	items: 1,
+  	loop: true,
+  	autoplay: true,
+  	autoplayHoverPause: true,
+  	autoplaySpeed: '1000',
+  });
+});
