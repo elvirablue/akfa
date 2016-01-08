5b0b98fe1b717646c0f7198ad65ab24c8552e228
Yuri Krutilov <ronnie57x@gmail.com>
2016-01-08 15:55:39 +0600

    desctop pages

--- app/js/main.js
+++ app/js/main.js
@@ -30,4 +30,37 @@ $(document).ready(function(){
   });
 });
 
+// Tabs
 
+(function($) {
+$(function() {
+
+  $('ul.tabs__caption').on('click', 'li:not(.active)', function() {
+    $(this)
+      .addClass('active').siblings().removeClass('active')
+      .closest('div.tabs').find('div.tabs__content').removeClass('active').eq($(this).index()).addClass('active');
+  });
+
+});
+})(jQuery);
+
+// Accordion
+
+$('.accordion__toggle').click(function(e) {
+    e.preventDefault();
+    var $this = $(this);
+    if ($this.next().hasClass('show')) {
+        $this.next().removeClass('show');
+        $this.next().slideUp(350);
+        $this.removeClass('active');
+
+    } else {
+        $this.parent().parent().find('.accordion__inner').removeClass('show');
+        $this.parent().parent().find('.accordion__inner').slideUp(350);
+        $this.next().toggleClass('show');
+        $this.next().slideToggle(350);
+        $this.removeClass('active');
+        $this.toggleClass('active');
+        $('.accordion__toggle').not(this).removeClass("active");
+    }
+});
